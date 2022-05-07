549d0e423354e6f16ae41c2a78d18b4f30bc5a25
Ben <ben.a.burgos@gmail.com>
2022-05-07 16:52:43 -0700

    sets up basic template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <h1>Giphy Generator</h1>
+      <button>Show me a Gif</button>
+      <h3>Use Giphy API to generate a random gif!</h3>
+      <img src="https://picsum.photos/400" alt="random" />
+      <h4>Powered by GIPHY</h4>
+    </div>
+  );
 }
 
 export default App;
